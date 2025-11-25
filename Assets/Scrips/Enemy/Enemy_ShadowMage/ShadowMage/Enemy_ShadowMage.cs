6e5ff01d13cc8bc63782a46d2d642a30730fa637
qingshan10086 <3506594891@qq.com>
2025-11-25 13:28:20 +0800

    暂时保存
    Co-Authored-By: hehe-6789 <180026256+hehe-6789@users.noreply.github.com>
    Co-Authored-By: wcsndm-1 <229734599+wcsndm-1@users.noreply.github.com>

--- Assets/Scrips/Enemy/Enemy_ShadowMage/ShadowMage/Enemy_ShadowMage.cs
+++ Assets/Scrips/Enemy/Enemy_ShadowMage/ShadowMage/Enemy_ShadowMage.cs
@@ -6,6 +6,10 @@ public class Enemy_ShadowMage : Enemy
 {
     public ShadowMageIdleState idleState {  get; private set; }//申明站立状态
     public ShadowMageAttack2State attack2State { get; private set; }//申明近身攻击（攻击2）状态
+    public ShadowMageAttack3State attack3State { get; private set; }//申明召唤小怪并位移（攻击3）状态
+    private float currentHealth;//当前血量百分数形式
+    private bool[] canAttack3 = new bool[4] { true, true, true, true };
+
 
 
     public ShadowMageAttack1State attack1State { get; private set; }//申明攻击1状态
@@ -25,6 +29,7 @@ public class Enemy_ShadowMage : Enemy
         idleState = new ShadowMageIdleState(this,stateMachine,"Idle",this);
         attack1State = new ShadowMageAttack1State(this, stateMachine, "Attack1", this);
         attack2State = new ShadowMageAttack2State(this, stateMachine, "Attack2", this);
+        attack3State = new ShadowMageAttack3State(this, stateMachine, "Attack3", this);
 
         stat=GetComponent<EnemyStats>();//获取敌人数据
     }
@@ -35,14 +40,58 @@ public class Enemy_ShadowMage : Enemy
         stateMachine.Initialize(idleState);//初始化状态
     }
 
+
+
     protected override void Update()
     {
         base.Update();
 
         Attack1();
+
+
+        Attack3();
+
     }
 
-   
+    private void Attack3()//判断能否进入攻击3状态
+    {
+        currentHealth = (float)stat.currentHealth / stat.maxHealth.GetValue();
+
+        if (canAttack3[0])
+        {
+            
+            if (currentHealth <= 0.8f && currentHealth >= 0.7f)
+            {
+                stateMachine.ChangeState(attack3State);
+                canAttack3[0] = false;
+            }
+        }
+        if (canAttack3[1])
+        {
+            if (currentHealth <=0.6f && currentHealth >= 0.5f)
+            {
+                stateMachine.ChangeState(attack3State);
+                canAttack3[1] = false;
+            }
+        }
+        if (canAttack3[2])
+        {
+            if (currentHealth <= 0.4f && currentHealth >= 0.3f)
+            {
+                stateMachine.ChangeState(attack3State);
+                canAttack3[2] = false;
+            }
+        }
+        if (canAttack3[3])
+        {
+            if (currentHealth <= 0.2f && currentHealth >= 0.1f)
+            {
+                stateMachine.ChangeState(attack3State);
+                canAttack3[3] = false;
+            }
+        }
+    }
+
     private void Attack1()//判断是否进入攻击1状态
     {
         if (!canAttack1)
